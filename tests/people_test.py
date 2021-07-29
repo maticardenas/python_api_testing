b73b5dd01751d399335ce938421c8d2e3b3880fc
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2021-07-29 23:10:10 +0200

    Adding soft assertion sample

--- tests/people_test.py
+++ tests/people_test.py
@@ -3,7 +3,7 @@ from typing import Dict, List
 from uuid import uuid4
 
 import requests
-from assertpy.assertpy import assert_that
+from assertpy.assertpy import assert_that, soft_assertions
 
 from config import BASE_URI
 
@@ -32,6 +32,21 @@ def test_new_person_can_be_added():
     peoples = requests.get(BASE_URI).json()
     is_new_user_created = search_created_user_in(peoples, unique_last_name)
     assert_that(is_new_user_created).is_not_empty()
+
+
+def test_read_all_has_kent_soft_assertion():
+    # given - when
+    # We use requests.get() with url to make a get request
+    response = requests.get(BASE_URI)
+    # response from requests has many useful properties
+    # we can assert on the response status code
+    # then
+    with soft_assertions():
+        assert_that(response.status_code).is_equal_to("any wrong code")
+        # We can get python dict as response by using .json() method
+        response_text = response.json()
+        first_names = [people['fname'] for people in response_text]
+        assert_that(first_names).contains('Kent')
 
 
 def test_created_person_can_be_deleted():
@@ -66,6 +81,7 @@ def create_new_person() -> str:
 
     # We use requests.post method with keyword params to make the request more readable
     response = requests.post(url=BASE_URI, data=payload, headers=headers)
+    response.headers
     assert_that(response.status_code, description='Person not created').is_equal_to(requests.codes.no_content)
     return unique_last_name
 
