e38d758dc0f175bceae2fbd89de7eac48d7d37f2
Matías Cárdenas <matiascardenas@Matiass-MacBook-Air.local>
2021-08-17 19:11:22 +0200

    Adding tests and functions for usage of json tests

--- tests/people_test.py
+++ tests/people_test.py
@@ -1,11 +1,15 @@
-from json import dumps
-from typing import Dict, List
+from json import dumps, loads
+import random
+from typing import Dict, List, Any, Tuple, Optional
 from uuid import uuid4
 
+import pytest
 import requests
 from assertpy.assertpy import assert_that, soft_assertions
+from jsonpath_ng import parse
 
 from config import BASE_URI
+from utils.file_reader import read_file
 
 
 def test_read_all_has_kent():
@@ -84,6 +88,74 @@ def create_new_person() -> str:
     response.headers
     assert_that(response.status_code, description='Person not created').is_equal_to(requests.codes.no_content)
     return unique_last_name
+
+
+@pytest.fixture
+def create_data():
+    payload = read_file('create_person.json')
+
+    random_no = random.randint(0, 1000)
+    last_name = f'Olabini{random_no}'
+
+    payload['lname'] = last_name
+    yield payload
+
+
+def test_person_can_be_added_with_a_json_template(create_data):
+    create_person_with_unique_last_name(create_data)
+
+    response = requests.get(BASE_URI)
+    peoples = loads(response.text)
+
+    # Get all last names for any object in the root array
+    # Here $ = root, [*] represents any element in the array
+    # Read full syntax: https://pypi.org/project/jsonpath-ng/
+    jsonpath_expr = parse("$.[*].lname")
+    result = [match.value for match in jsonpath_expr.find(peoples)]
+
+    expected_last_name = create_data['lname']
+    assert_that(result).contains(expected_last_name)
+
+
+def create_person_with_unique_last_name(body: Optional[Dict[str, str]] = None):
+    payload = _get_person_payload(body)
+    unique_last_name = get_prop_from_str_payload(payload, "lname")
+    # Setting default headers to show that the client accepts json
+    # And will send json in the headers
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+
+    # We use requests.post method with keyword params to make the request more readable
+    response = requests.post(url=BASE_URI, data=payload, headers=headers)
+    assert_that(response.status_code, description='Person not created').is_equal_to(requests.codes.no_content)
+    return unique_last_name
+
+
+def _get_person_payload(body: Dict[str, Any]) -> Dict[str, Any]:
+    if not body:
+        # Ensure a user with a unique last name is created everytime the test runs
+        # Note: json.dumps() is used to convert python dict to json string
+        unique_last_name = f'User {str(uuid4())}'
+        payload = dumps({
+            'fname': 'New',
+            'lname': unique_last_name
+        })
+    else:
+        payload = dumps(body)
+
+    return payload
+
+
+def get_prop_from_str_payload(payload: str, property: str) -> Any:
+    json_obj = loads(payload)
+    try:
+        prop_value = json_obj[property]
+    except:
+        print(f"Property {property} does not exist in provided payload")
+
+    return prop_value
 
 
 def search_created_user_in(peoples: Dict[str, str], last_name: str) -> List[Dict[str, str]]:
