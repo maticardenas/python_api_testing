85bc5e8d59dff3a0b70973207e656bb1049f2600
Matías Cárdenas <matiascardenas@matiass-macbook-air.home>
2021-08-27 19:59:46 +0200

    Adding types

--- clients/people/people_client.py
+++ clients/people/people_client.py
@@ -1,12 +1,14 @@
 from json import dumps
-from typing import Optional, Dict, Any, Tuple
+from typing import Optional, Dict, Any, Tuple, TYPE_CHECKING
 from uuid import uuid4
-
-from requests import Response
 
 from clients.people.base_client import BaseClient
 from config import BASE_URI
+from utils import request
 from utils.request import APIRequest
+
+if TYPE_CHECKING:
+    from utils.request import Response
 
 
 class PeopleClient(BaseClient):
@@ -16,12 +18,12 @@ class PeopleClient(BaseClient):
         self.base_url = BASE_URI
         self.request = APIRequest()
 
-    def create_person(self, body: Optional[Dict[Any, Any]] = None) -> Tuple[str, Response]:
+    def create_person(self, body: Optional[Dict[Any, Any]] = None) -> Tuple[str, "Response"]:
         last_name, response = self.__create_person_with_unique_last_name(body)
         return last_name, response
 
-    def __create_person_with_unique_last_name(self, body=None):
-        if body is None:
+    def __create_person_with_unique_last_name(self, body: Optional[Dict[Any, Any]] = None) -> Tuple[str, "Response"]:
+        if not body:
             last_name = f'User {str(uuid4())}'
             payload = dumps({
                 'fname': 'New',
@@ -34,15 +36,15 @@ class PeopleClient(BaseClient):
         response = self.request.post(self.base_url, payload, self.headers)
         return last_name, response
 
-    def read_one_person_by_id(self, person_id):
+    def read_one_person_by_id(self, person_id: int) -> "Response":
         pass
 
-    def read_all_persons(self):
+    def read_all_persons(self) -> "Response":
         return self.request.get(self.base_url)
 
-    def update_person(self):
+    def update_person(self) -> "Response":
         pass
 
-    def delete_person(self, person_id):
+    def delete_person(self, person_id: int) -> "Response":
         url = f'{BASE_URI}/{person_id}'
         return self.request.delete(url)
