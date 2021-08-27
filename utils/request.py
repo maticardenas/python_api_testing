452fa5d1399b4b9054f017c007fc4238b8bcc484
Matías Cárdenas <matiascardenas@matiass-macbook-air.home>
2021-08-27 20:11:12 +0200

    Moving data fixture to independent module

---
+++ utils/request.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+from typing import Dict, Any
+
+import requests
+
+
+@dataclass
+class Response:
+    status_code: int
+    text: str
+    as_dict: object
+    headers: dict
+
+
+class APIRequest:
+    def get(self, url: str) -> Response:
+        response = requests.get(url)
+        return self.__get_responses(response)
+
+    def post(self, url: str, payload: Dict[Any, Any], headers: Dict[str, str]) -> Response:
+        response = requests.post(url, data=payload, headers=headers)
+        return self.__get_responses(response)
+
+    def delete(self, url: str) -> Response:
+        response = requests.delete(url)
+        return self.__get_responses(response)
+
+    def __get_responses(self, response: "requests.models.Response") -> Response:
+        status_code = response.status_code
+        text = response.text
+
+        try:
+            as_dict = response.json()
+        except Exception:
+            as_dict = {}
+
+        headers = response.headers
+
+        return Response(
+            status_code, text, as_dict, headers
+        )
